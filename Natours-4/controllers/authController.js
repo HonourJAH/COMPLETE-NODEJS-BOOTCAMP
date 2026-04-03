d2f907cca6c3d50d590332899dd583d989638226
HonourJAH <onojahmicheal@gmail.com>
2026-04-03 02:15:09 +0100

    added user's authentication

--- Natours-4/controllers/authController.js
+++ Natours-4/controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -16,6 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirmed: req.body.passwordConfirmed,
+    passwordChangedAt: req.body.passwordChangedAt,
   });
 
   const token = signToken(newUser._id);
@@ -63,7 +65,6 @@ exports.protect = catchAsync(async (req, res, next) => {
     token = req.headers.authorization.split(' ')[1];
   }
 
-  console.log(token);
   if (!token) {
     return next(
       new AppError('You are not logged in! Please log in to get access.', 401),
@@ -71,20 +72,27 @@ exports.protect = catchAsync(async (req, res, next) => {
   }
 
   // // 2) Verification token
-  // const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
-  // // 3) Check if user still exists
-  // const currentUser = await User.findById(decoded.id);
-  // if (!currentUser) {
-  //   return next(
-  //     new AppError(
-  //       'The user belonging to this token does no longer exist.',
-  //       401,
-  //     ),
-  //   );
-  // }
+  // 3) Check if user still exists
+  const freshUser = await User.findById(decoded.id);
+  if (!freshUser) {
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exist.',
+        401,
+      ),
+    );
+  }
+
+  // 4) Check if user changed password after the token was issued
+  if (freshUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401),
+    );
+  }
 
   // // GRANT ACCESS TO PROTECTED ROUTE
-  // req.user = currentUser;
+  req.user = freshUser;
   next();
 });
--- Natours-4/models/userModel.js
+++ Natours-4/models/userModel.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: [8, 'Password must be at least 8 characters long'],
-    selfelect: false,
+    select: false,
   },
   passwordConfirmed: {
     type: String,
@@ -32,6 +32,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same!',
     },
   },
+  passwordChangedAt: Date,
 });
 
 userSchema.pre('save', function (next) {
@@ -51,6 +52,19 @@ userSchema.methods.correctPassword = async function (
   userPassword,
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10,
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+  // False means NOT changed
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
